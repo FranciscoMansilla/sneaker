9c00a7d4533df31d5615c3207592c9a21b3c37dc
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-10-09 16:06:53 -0300

    add endpoints /login /register

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
+require('colors')
 const express = require('express')
 const router = express.Router()
 const {usersModel} = require('../models')
+const jwt = require('jsonwebtoken')
+const privateKey = 'contraseasecreta'
+const { v4: uuidv4 } = require('uuid')
+const {getToken, getTokenData} = require('../config/jwt.config')
 
 router.get('/',async(req,res)=>{
   const data = await usersModel.find({})
@@ -15,6 +20,78 @@ router.get('/:id',async(req,res)=>{
     res.send({error})
   }
 })
+
+router.post('/login', async(req,res)=>{
+  const {email, password} = req.body
+  const user = await usersModel.findOne({email})
+  if(user){
+    const result = await user.comparePassword(password)
+    console.log(result, 'resultado'.green.inverse)
+    if(result){
+      const token = getToken({user: user.email, code:user.code})
+      return res.send({
+        succes: true,
+        msg: 'logueado',
+        token
+      })
+    }
+    return res.send({
+      succes: false,
+      msg: 'Contraseña incorrecta!',
+    })
+  } else {
+    res.send({
+      succes: false,
+      msg: 'Usuario no registrado!'
+    })
+  }
+    // const token = jwt.sign({
+    //   exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    //   data: email
+    // }, privateKey);
+    // res.send({
+    //   succes: true,
+    //   token
+    // })
+})
+
+router.post('/register',async(req,res)=>{
+  try {
+    const {email, name, password} = req.body
+    let user = await usersModel.findOne({email}) || null;
+    if(user){
+      return res.send({
+        succes: false,
+        msg: 'El usuario ya existe'
+      })
+    }
+    const code = uuidv4()
+    const newUser = await usersModel.create({email, password, name, code})
+    if(!newUser){
+      res.send({
+        succes: false,
+        msg: 'Error al registrar usuario',
+        error: err
+      })
+    } else {
+      const token = getToken({email, code})
+      res.send({
+        succes: true,
+        msg: 'Usuario Registrado!',
+        token,
+        newUser
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.send({
+      succes: false,
+      msg: 'Error al registrar',
+      error: error
+    })
+  }
+})
+
 router.post('/',async(req,res)=>{
   const {body} = req
   try {
